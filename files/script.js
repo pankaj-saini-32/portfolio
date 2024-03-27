a3c7198d7758906352dbad0382a33e8ee3c6aa70
surendrakumar6350 <kamleshksks456@gmail.com>
2024-03-27 11:43:12 -0700

    sdfsdf

--- files/script.js
+++ files/script.js
@@ -1,3 +1,12 @@
+let email = document.querySelector(".email");
+let message = document.querySelector(".message");
+var x = document.getElementById("snackbar");
+
+function myFunction() {
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
+
 
 var typed = new Typed(".text", {
     strings: ["Advisor"  , "Analysist"],
@@ -17,3 +26,30 @@ window.addEventListener("scroll",() =>{
         toTop.classList.remove("active");
     }
 })
+
+
+const fnc = ()=> {
+    if(email.value.length < 1 || message.value.length < 1) {
+        x.innerHTML = "Name and message are requied🙄🙄"
+        myFunction();
+    }
+    else {
+    (async () => {
+        const rawResponse = await fetch('https://webscraping-lmk3.onrender.com/p', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({a: email.value, b: message.value})
+        });
+        const content = await rawResponse.json();
+      if(content.success) {
+        x.innerHTML = "Message send successfully🎉"
+        myFunction();
+      }
+      })();
+    }
+}
+
+
